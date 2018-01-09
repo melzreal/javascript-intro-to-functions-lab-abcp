34dbef9a3d075b80de626f83fc05f13791fdd50a
Melissa Real <melissa.real@hotmail.com>
2018-01-09 15:46:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,17 @@ function logShout(string){
 
 function sayHiToGrandma(string){
   
-if(string === string.toLowerCase){
-  return("I can't hear you")
+ if(string === string.toLowerCase()){
+  return("I can't hear you!")
 } 
+  else if(string === string.toUpperCase()){
+    return("YES INDEED!")
+  }
+  else if(string === "I love you, Grandma") {
+    
+    return("I lvoe you, too.")
+  }
+  
   
 }
 
